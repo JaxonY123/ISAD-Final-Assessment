67c79cac0f542b7d6b16c44748258712f266cb12
Jaxon Young <21549827@student.curtin.edu.au>
2023-05-11 17:24:30 +0800

    Added getSeaon function. Basic version that gives the season using the country and month parameters (besides Australia).

--- weather.py
+++ weather.py
@@ -1,4 +1,45 @@
 #
-# weather.py - 
-# 
+# weather.py - production code for ISAD Assignment
+#  
+
+def getSeason(country, month):
+    """
+    Displays the name and a graphic of the season in a given country during
+    a given month.
+    """
+    season = ""
+    if country == "Australia":
+        pass
+    elif country == "Spain" or country == "Japan":
+        if month == "December" or month == "January" or month == "February":
+            season = "Winter"
+        elif month == "March" or month == "April" or month == "May":
+            season = "Spring"
+        elif month == "June" or month == "July" or month == "August":
+            season = "Summer"
+        else:
+            season = "Autumn"
+
+    elif country == "Mauritius":
+        if month == "May":
+            season = "Autumn"
+        elif month == "June" or month == "July" or month == "August" or month == "September":
+            season = "Winter"
+        elif month == "October":
+            season = "Spring"
+        else:
+            season = "Summer"
+
+    elif country == "Malaysia" or country == "Sri Lanka":
+        if month == "December" or month == "January" or month == "February":
+            season = "Northeast Monsoon"
+        elif month == "May" or month == "September":
+            season = "Southeast Monsoon"
+        else:
+            season = "Inter-monsoon"
+    print(season)
+
+
+    
+
 
