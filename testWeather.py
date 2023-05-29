42f3bd33130723e9a88bdbbf69726280d5662cb3
Jaxon Young <21549827@student.curtin.edu.au>
2023-05-29 19:58:20 +0800

    Added test code for getSeason

--- testWeather.py
+++ testWeather.py
@@ -2,114 +2,138 @@ import sys
 import io
 import weather
 import unittest
+import matplotlib.pyplot as plt
 
 class TestWeather(unittest.TestCase):
     def setUp(self):
+        plt.ion()
         self.savedSTDIN = sys.stdin
         self.savedSTDOUT = sys.stdout
 
     def tearDown(self):
+        plt.ioff()
         sys.stdin = self.savedSTDIN
         sys.stdout = self.savedSTDOUT
 
     def testInputCheck1(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("hello\nhi\n")          #ADD CORRECTION INPUT TO TABLES
         self.assertEqual(weather.inputCheck("", ["Hi", "Goodbye"]), "Hi")
     def testInputCheck2(self):                            #ADD MESSAGES AT END
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("Young\n")
         self.assertEqual(weather.inputCheck("", ["Jaxon", "Young"]), "Young") #add case sensitive stuff
 
     def testTimeCheck1(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("9am\n3pm\n")
         self.assertEqual(weather.timeCheck("", ["9pm", "3pm"]), "3pm")
     def testTimeCheck2(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("9am\n")
         self.assertEqual(weather.timeCheck("", ["9am", "3pm"]), "9am")
     
     def testTempCheck1(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("nine eight two seven\n9827")
         self.assertEqual(weather.tempCheck(""), 9827.0)
     def testTempCheck2(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
         sys.stdin = io.StringIO("9827\n")
         self.assertEqual(weather.tempCheck(""), 9827.0)
 
     def testGetSeason(self):
         countries = ["Spain", "Japan", "Mauritius", "Malaysia", "Sri Lanka"]
         months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        expectedSeason = {
+        expectedSeasons = {
                 "Spain": {
-                "December": ["Winter", "winter.png"],
-                "January": ["Winter", "winter.png"],
-                "February": ["Winter", "winter.png"],
-                "March": ["Spring", "spring.png"],
-                "April": ["Spring", "spring.png"],
-                "May": ["Spring", "spring.png"],
-                "June": ["Summer", "summer.png"],
-                "July": ["Summer", "summer.png"],
-                "August": ["Summer", "summer.png"],
-                "September": ["Autumn", "autumn.png"],
-                "October": ["Autumn", "autumn.png"],
-                "November": ["Autumn", "autumn.png"],
+                "December": "Winter",
+                "January": "Winter",
+                "February": "Winter",
+                "March": "Spring",
+                "April": "Spring",
+                "May": "Spring", 
+                "June": "Summer",
+                "July": "Summer", 
+                "August": "Summer", 
+                "September": "Autumn",
+                "October": "Autumn", 
+                "November": "Autumn",
             },
         "Japan": {
-                "December": ["Winter", "winter.png"],
-                "January": ["Winter", "winter.png"],
-                "February": ["Winter", "winter.png"],
-                "March": ["Spring", "spring.png"],
-                "April": ["Spring", "spring.png"],
-                "May": ["Spring", "spring.png"],
-                "June": ["Summer", "summer.png"],
-                "July": ["Summer", "summer.png"],
-                "August": ["Summer", "summer.png"],
-                "September": ["Autumn", "autumn.png"],
-                "October": ["Autumn", "autumn.png"],
-                "November": ["Autumn", "autumn.png"],
+                "December": "Winter",
+                "January": "Winter",
+                "February": "Winter",
+                "March": "Spring", 
+                "April": "Spring", 
+                "May": "Spring", 
+                "June": "Summer",
+                "July": "Summer",
+                "August": "Summer", 
+                "September": "Autumn",
+                "October": "Autumn",
+                "November": "Autumn",
             },
         "Mauritius": {
-                "May": ["Autumn", "autumn.png"],
-                "June": ["Winter", "winter.png"],
-                "July": ["Winter", "winter.png"],
-                "August": ["Winter", "winter.png"],
-                "September": ["Winter", "winter.png"],
-                "October": ["Spring", "spring.png"],
-                "November": ["Spring", "spring.png"],
-                "December": ["Summer", "summer.png"],
-                "January": ["Summer", "summer.png"],
-                "February": ["Summer", "summer.png"],
-                "March": ["Summer", "summer.png"],
-                "April": ["Summer", "summer.png"],
+                "May": "Autumn",
+                "June": "Winter", 
+                "July": "Winter", 
+                "August": "Winter", 
+                "September": "Winter",
+                "October": "Spring", 
+                "November": "Spring", 
+                "December": "Summer", 
+                "January": "Summer", 
+                "February": "Summer", 
+                "March": "Summer",
+                "April": "Summer", 
             },
         "Malaysia": {
-                "December": ["Northeast Monsoon", "monsoon.png"],
-                "January": ["Northeast Monsoon", "monsoon.png"],
-                "February": ["Northeast Monsoon", "monsoon.png"],
-                "May": ["Southeast Monsoon", "monsoon.png"],
-                "September": ["Southeast Monsoon", "monsoon.png"],
-                "March": ["Inter-monsoon", "inter-monsoon.png"],
-                "April": ["Inter-monsoon", "inter-monsoon.png"],
-                "June": ["Inter-monsoon", "inter-monsoon.png"],
-                "July": ["Inter-monsoon", "inter-monsoon.png"],
-                "August": ["Inter-monsoon", "inter-monsoon.png"],
-                "October": ["Inter-monsoon", "inter-monsoon.png"],
-                "November": ["Inter-monsoon", "inter-monsoon.png"],
+                "December": "Northeast Monsoon",
+                "January": "Northeast Monsoon",
+                "February": "Northeast Monsoon", 
+                "May": "Southeast Monsoon", 
+                "September": "Southeast Monsoon", 
+                "March": "Inter-monsoon", 
+                "April": "Inter-monsoon", 
+                "June": "Inter-monsoon",
+                "July": "Inter-monsoon", 
+                "August": "Inter-monsoon", 
+                "October": "Inter-monsoon", 
+                "November": "Inter-monsoon", 
             },
         "Sri Lanka": {
-                "December": ["Northeast Monsoon", "monsoon.png"],
-                "January": ["Northeast Monsoon", "monsoon.png"],
-                "February": ["Northeast Monsoon", "monsoon.png"],
-                "May": ["Southeast Monsoon", "monsoon.png"],
-                "September": ["Southeast Monsoon", "monsoon.png"],
-                "March": ["Inter-monsoon", "inter-monsoon.png"],
-                "April": ["Inter-monsoon", "inter-monsoon.png"],
-                "June": ["Inter-monsoon", "inter-monsoon.png"],
-                "July": ["Inter-monsoon", "inter-monsoon.png"],
-                "August": ["Inter-monsoon", "inter-monsoon.png"],
-                "October": ["Inter-monsoon", "inter-monsoon.png"],
-                "November": ["Inter-monsoon", "inter-monsoon.png"],
+                "December": "Northeast Monsoon",
+                "January": "Northeast Monsoon", 
+                "February": "Northeast Monsoon",
+                "May": "Southeast Monsoon", 
+                "September": "Southeast Monsoon", 
+                "March": "Inter-monsoon", 
+                "April": "Inter-monsoon", 
+                "June": "Inter-monsoon", 
+                "July": "Inter-monsoon", 
+                "August": "Inter-monsoon", 
+                "October": "Inter-monsoon", 
+                "November": "Inter-monsoon",
             }
         }
+        
+        for country in countries:
+            for month in months:
+                with self.subTest(country=country, month=month):
+                    capOut = io.StringIO()
+                    sys.stdout = capOut
+                    season = weather.getSeason(country, month)
+                    sys.stdout = self.savedSTDOUT
 
-        capOut = io.StringIO()
-        sys.
+                    expectedSeason = expectedSeasons[country][month]
+                    self.assertEqual(capOut.getvalue().strip(), f"During {month} in {country} the season is {expectedSeason}.")
+
 if __name__ == '__main__':
     unittest.main()
